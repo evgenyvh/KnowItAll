183afcd3f5ec1fb42084fd532ca7fde885780f81
Rowan van laar <534571@edu.rocmn.nl>
2019-06-19 13:10:35 +0200

    javascript update

--- kalender.php
+++ kalender.php
@@ -1,111 +1,97 @@
-<?php
-// Set your timezone
-date_default_timezone_set('Asia/Tokyo');
-// Get prev & next month
-if (isset($_GET['ym'])) {
-    $ym = $_GET['ym'];
-} else {
-    // This month
-    $ym = date('Y-m');
-}
-// Check format
-$timestamp = strtotime($ym . '-01');
-if ($timestamp === false) {
-    $ym = date('Y-m');
-    $timestamp = strtotime($ym . '-01');
-}
-// Today
-$today = date('Y-m-j', time());
-// For H3 title
-$html_title = date('Y / m', $timestamp);
-// Create prev & next month link     mktime(hour,minute,second,month,day,year)
-$prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
-$next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
-// You can also use strtotime!
-// $prev = date('Y-m', strtotime('-1 month', $timestamp));
-// $next = date('Y-m', strtotime('+1 month', $timestamp));
-// Number of days in the month
-$day_count = date('t', $timestamp);
-
-// 0:Sun 1:Mon 2:Tue ...
-$str = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
-//$str = date('w', $timestamp);
-// Create Calendar!!
-$weeks = array();
-$week = '';
-// Add empty cell
-$week .= str_repeat('<td></td>', $str);
-for ( $day = 1; $day <= $day_count; $day++, $str++) {
-
-    $date = $ym . '-' . $day;
-
-    if ($today == $date) {
-        $week .= '<td class="today">' . $day;
-    } else {
-        $week .= '<td>' . $day;
-    }
-    $week .= '</td>';
-
-    // End of the week OR End of the month
-    if ($str % 7 == 6 || $day == $day_count) {
-        if ($day == $day_count) {
-            // Add empty cell
-            $week .= str_repeat('<td></td>', 6 - ($str % 7));
-        }
-        $weeks[] = '<tr>' . $week . '</tr>';
-        // Prepare for new week
-        $week = '';
-    }
-}
-?>
-
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script type="text/javascript" src="script.js"></script>
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <title>Kalender</title>
-    <style>
-
-    </style>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <title>PHP Calendar</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <script src="script.js"></script>
 </head>
-<body>
+<body onload="startTime()">
 
 <ul class="topnav">
-    <li><a class="active" href="index.php">Home</a></li>
-    <li><a href="kalender.php">Kalender</a></li>
-    <li><a href="contact.html">Contact</a></li>
-    <li><a href="InlogRegister/login.php">Log in/uit</a></li>
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="#calender">Kalender</a></li>
+    <li><a href="#contact">Contact</a></li>
 </ul>
-
-
+<div class="container">
+    <div class="card">
+        <div class="front">
+            <div class="contentfront">
+                <div class="month">
+                    <table>
+                        <tr class="orangeTr">
+                            <th>M</th>
+                            <th>T</th>
+                            <th>W</th>
+                            <th>T</th>
+                            <th>F</th>
+                            <th>S</th>
+                            <th>S</th>
+                        </tr>
+                        <tr class="whiteTr">
+                            <th></th>
+                            <th>1</th>
+                            <th>2</th>
+                            <th>3</th>
+                            <th>4</th>
+                            <th>5</th>
+                            <th>6</th>
+                        </tr>
+                        <tr class="whiteTr">
+                            <th>7</th>
+                            <th>8</th>
+                            <th>9</th>
+                            <th>10</th>
+                            <th>11</th>
+                            <th>12</th>
+                            <th>13</th>
+                        </tr>
+                        <tr class="whiteTr">
+                            <th>14</th>
+                            <th>15</th>
+                            <th>16</th>
+                            <th>17</th>
+                            <th>18</th>
+                            <th>19</th>
+                            <th>20</th>
+                        </tr>
+                        <tr class="whiteTr">
+                            <th>21</th>
+                            <th>22</th>
+                            <th>23</th>
+                            <th>24</th>
+                            <th>25</th>
+                            <th>26</th>
+                            <th>27</th>
+                        </tr>
+                        <tr class="whiteTr">
+                            <th>28</th>
+                            <th>29</th>
+                            <th>30</th>
+                            <th>31</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </table>
+                </div>
+                <div class="date">
+                    <div class="datecont">
+                        <div id="date"></div>
+                        <div id="day"></div>
+                        <div id="month"></div>
+                        <i class="fa fa-pencil edit" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="footer">
     <h2>KnowItAll</h2>
     <h4>Copyright © 2019 KnowItAll Productions</h4>
 </div>
-    <div class="container">
-        <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
-        <table class="table table-bordered">
-            <tr>
-                <th>Z</th>
-                <th>M</th>
-                <th>D</th>
-                <th>W</th>
-                <th>D</th>
-                <th>V</th>
-                <th>Z</th>
-            </tr>
-            <?php
-            foreach ($weeks as $week) {
-                echo $week;
-            }
-            ?>
-        </table>
-    </div>
 </body>
 </html>
-
